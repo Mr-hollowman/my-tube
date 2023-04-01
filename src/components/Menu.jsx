50bd4ad854192a8a2f4d6846fcbf0d07dd962f4d
mr-hollowman <selvamani835@gmail.com>
2023-04-01 19:38:52 +0530

    app setup

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    flex:1;
+    background:#202020;
+    height:100vh;
+    color:white;
+`;
+
+const Wrapper = styled.div`
+    padding:18px 26px;
+`;
+export default function Menu() {
+  return (
+    <Container>
+        <Wrapper>
+            items
+        </Wrapper>
+    </Container>
+  )
+}
